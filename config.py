3fec6ea2aae09432f44e304b972382d09f15744d
shinu5516 <tgmusic5516@gmail.com>
2025-05-23 22:12:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
+    "https://github.com/shinu5516/AviaxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
