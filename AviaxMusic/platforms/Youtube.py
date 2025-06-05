7611040ccccb54d7f9ee99e9736d98f25c8881bd
Vivek Raj Roy <vivekalwaysfun1@gmail.com>
2025-06-05 18:44:00 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -43,7 +43,7 @@ def extract_video_id(link: str) -> str:
 
 
 def api_dl(video_id: str) -> str | None:
-    api_url = f"{API_BASE_URL}:8080/download/song/{video_id}?key={API_KEY}"
+    api_url = f"{API_BASE_URL}/download/song/{video_id}?key={API_KEY}"
     file_path = os.path.join("downloads", f"{video_id}.mp3")
 
     # ✅ Check if already downloaded
